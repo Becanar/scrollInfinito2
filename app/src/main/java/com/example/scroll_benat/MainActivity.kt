35cf159196f2cd62c082070eb3f383e77c9ef888
Becanar <116558378+Becanar@users.noreply.github.com>
2024-11-01 18:21:48 +0100

    JavadocHecho

--- app/src/main/java/com/example/scroll_benat/MainActivity.kt
+++ app/src/main/java/com/example/scroll_benat/MainActivity.kt
@@ -144,7 +144,11 @@ class MainActivity : AppCompatActivity() {
             updateNoTasksVisibility()
         }
     }
-
+    /**
+     * Método que configura el gesto de deslizar para eliminar tareas del RecyclerView.
+     * Utiliza un ItemTouchHelper para gestionar los eventos de deslizamiento y permite
+     * a los usuarios eliminar tareas deslizando hacia la izquierda o hacia la derecha.
+     */
     private fun attachSwipeToDelete() {
         val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
             override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
